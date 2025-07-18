b9591db9a70949368f37c47608061190d56e1b91
rover0811 <rover0811@hotmail.com>
2025-07-18 23:22:26 +0900

    [Test] 3nd test settings, limits and reservations have same cpus core

--- chapter-13/project/main.py
+++ chapter-13/project/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -6,10 +8,19 @@ import redis
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-import os
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-r = redis.Redis(host=REDIS_HOST, port=6379, db=0, decode_responses=True)
+redis_pool = redis.connection.BlockingConnectionPool(
+    host=REDIS_HOST,
+    port=6379,
+    db=0,
+    decode_responses=True,
+    max_connections=500,  # RPS 5000 대응
+    timeout=20,
+    retry_on_timeout=True,
+    socket_keepalive=True
+)
+r = redis.Redis(connection_pool=redis_pool)
 
 @app.get("/health")
 async def health_check():
