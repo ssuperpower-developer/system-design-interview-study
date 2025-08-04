de684a8c877e28f4d1e9099bb3a2b35054f7e32e
SSUN <rlatjsghks4647@naver.com>
2025-08-05 01:27:43 +0900

    [ADD] list, set 실습

--- fastcampus2/src/main/java/org/Main.java
+++ fastcampus2/src/main/java/org/Main.java
@@ -14,34 +14,65 @@ public class Main {
         try (var jedisPool = new JedisPool("localhost", 6379)) {
             try (Jedis jedis = jedisPool.getResource()) {
                 // string
-                jedis.set("users:300:email", "hi@naver.com");
-                jedis.set("users:300:name", "kim");
-                jedis.set("users:300:age", "25");
+//                jedis.set("users:300:email", "hi@naver.com");
+//                jedis.set("users:300:name", "kim");
+//                jedis.set("users:300:age", "25");
+//
+//                String userEmail = jedis.get("users:300:email");
+//                System.out.println(userEmail);
+//
+//                List<String> userInfo = jedis.mget("users:300:email", "users:300:name", "users:300:age");
+//                userInfo.forEach(System.out::println);
+//
+//                long counter = jedis.incr("counter");
+//                System.out.println(counter);
+//
+//                counter = jedis.incrBy("counter", 10L);
+//                System.out.println(counter);
+//
+//                counter = jedis.decr("counter");
+//                System.out.println(counter);
+//
+//                counter = jedis.decrBy("counter", 10L);
+//                System.out.println(counter);
+//
+//                Pipeline pipeline = jedis.pipelined();
+//                pipeline.set("users:400:email", "hi@naver.com");
+//                pipeline.set("users:400:name", "kim");
+//                pipeline.set("users:400:age", "25");
+//                List<Object> objects = pipeline.syncAndReturnAll();
+//                objects.forEach(i -> System.out.println(i.toString()));
 
-                String userEmail = jedis.get("users:300:email");
-                System.out.println(userEmail);
+                // list
+                // 1. stack
+                jedis.rpush("stack1", "aaaa");
+                jedis.rpush("stack1", "bbbb");
+                jedis.rpush("stack1", "cccc");
 
-                List<String> userInfo = jedis.mget("users:300:email", "users:300:name", "users:300:age");
-                userInfo.forEach(System.out::println);
+                List<String> stack1 = jedis.lrange("stack1", 0, -1);
+                stack1.forEach(System.out::println);
 
-                long counter = jedis.incr("counter");
-                System.out.println(counter);
+                while(true) {
+                    List<String> blpop = jedis.blpop(10, "queue:blocking");
+                    if(blpop != null) {
+                        blpop.forEach(System.out::println);
+                    }
+                }
 
-                counter = jedis.incrBy("counter", 10L);
-                System.out.println(counter);
+                // set
+                jedis.sadd("users:500:follow", "100", "200", "300");
+                jedis.srem("users:500:follow", "100");
 
-                counter = jedis.decr("counter");
-                System.out.println(counter);
+                Set<String> members = jedis.smembers("users:500:follow");
+                members.forEach(System.out::println);
 
-                counter = jedis.decrBy("counter", 10L);
-                System.out.println(counter);
+                System.out.println(jedis.sismember("users:500:follow", "200"));
+                System.out.println(jedis.sismember("users:500:follow", "120"));
 
-                Pipeline pipeline = jedis.pipelined();
-                pipeline.set("users:400:email", "hi@naver.com");
-                pipeline.set("users:400:name", "kim");
-                pipeline.set("users:400:age", "25");
-                List<Object> objects = pipeline.syncAndReturnAll();
-                objects.forEach(i -> System.out.println(i.toString()));
+                System.out.println(jedis.scard("users:500:follow"));
+
+                Set<String> sinter = jedis.sinter("users:500:follow", "users:100:follow");
+                sinter.forEach(System.out::println);
             }
         }
     }
