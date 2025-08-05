8c4f8cdef198df0d5ac035ba43600df983b4711c
SSUN <rlatjsghks4647@naver.com>
2025-08-05 17:33:33 +0900

    [INIT] cache 실습 세팅

---
+++ fastcampus2/jediscache/src/main/java/com/example/jediscache/JediscacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.jediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JediscacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JediscacheApplication.class, args);
+	}
+
+}
