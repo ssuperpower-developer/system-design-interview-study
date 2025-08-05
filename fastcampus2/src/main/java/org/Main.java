bcdbf3d6094e1ebf7d09799c1267480b7ff02e43
SSUN <rlatjsghks4647@naver.com>
2025-08-05 10:32:12 +0900

    [ADD] hash 실습

--- fastcampus2/src/main/java/org/Main.java
+++ fastcampus2/src/main/java/org/Main.java
@@ -4,7 +4,9 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Pipeline;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
@@ -45,34 +47,51 @@ public class Main {
 
                 // list
                 // 1. stack
-                jedis.rpush("stack1", "aaaa");
-                jedis.rpush("stack1", "bbbb");
-                jedis.rpush("stack1", "cccc");
-
-                List<String> stack1 = jedis.lrange("stack1", 0, -1);
-                stack1.forEach(System.out::println);
-
-                while(true) {
-                    List<String> blpop = jedis.blpop(10, "queue:blocking");
-                    if(blpop != null) {
-                        blpop.forEach(System.out::println);
-                    }
-                }
+//                jedis.rpush("stack1", "aaaa");
+//                jedis.rpush("stack1", "bbbb");
+//                jedis.rpush("stack1", "cccc");
+//
+//                List<String> stack1 = jedis.lrange("stack1", 0, -1);
+//                stack1.forEach(System.out::println);
+//
+//                while(true) {
+//                    List<String> blpop = jedis.blpop(10, "queue:blocking");
+//                    if(blpop != null) {
+//                        blpop.forEach(System.out::println);
+//                    }
+//                }
 
                 // set
-                jedis.sadd("users:500:follow", "100", "200", "300");
-                jedis.srem("users:500:follow", "100");
+//                jedis.sadd("users:500:follow", "100", "200", "300");
+//                jedis.srem("users:500:follow", "100");
+//
+//                Set<String> members = jedis.smembers("users:500:follow");
+//                members.forEach(System.out::println);
+//
+//                System.out.println(jedis.sismember("users:500:follow", "200"));
+//                System.out.println(jedis.sismember("users:500:follow", "120"));
+//
+//                System.out.println(jedis.scard("users:500:follow"));
+//
+//                Set<String> sinter = jedis.sinter("users:500:follow", "users:100:follow");
+//                sinter.forEach(System.out::println);
 
-                Set<String> members = jedis.smembers("users:500:follow");
-                members.forEach(System.out::println);
+                // hash
+                jedis.hset("users:2:info", "name", "kim");
+                var userInfo = new HashMap<String, String>();
+                userInfo.put("email", "hi@naver.com");
+                userInfo.put("phone", "010-XXXX-XXXX");
 
-                System.out.println(jedis.sismember("users:500:follow", "200"));
-                System.out.println(jedis.sismember("users:500:follow", "120"));
+                jedis.hset("users:2:info", userInfo);
 
-                System.out.println(jedis.scard("users:500:follow"));
+                jedis.hdel("users:2:info", "phone");
 
-                Set<String> sinter = jedis.sinter("users:500:follow", "users:100:follow");
-                sinter.forEach(System.out::println);
+                System.out.println(jedis.hget("users:2:info", "email"));
+                Map<String,String> user2Info = jedis.hgetAll("users:2:info");
+                user2Info.forEach((k, v) -> System.out.println("key:" + k + ",value:" + v));
+
+                jedis.hincrBy("users:2:info", "visits", 1);
+
             }
         }
     }
