c5d537e1ed0d85d0efa9056f69d7477974804245
Gusionling <isdnhk@naver.com>
2025-08-09 11:59:43 +0900

    feat : error 응답 구성

--- 접속자 대기열 시스템/Webflux/src/main/java/com/examp/Webflux/service/UserQueueService.java
+++ 접속자 대기열 시스템/Webflux/src/main/java/com/examp/Webflux/service/UserQueueService.java
@@ -1,5 +1,7 @@
 package com.examp.Webflux.service;
 
+import com.examp.Webflux.exception.ApplicationException;
+import com.examp.Webflux.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -23,14 +25,17 @@ public class UserQueueService {
     private final String USER_WAIT_QUEUE_KEY = "users:queue:%s:wait";
 
 
-    public Mono<Long> registerWaitQueue(final String queue,final Long userId){
-        var unixTimeStamp = Instant.now().getEpochSecond();
+    public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
+        // unix timestamp
+        long unixTimeStamp = Instant.now().getEpochSecond();
         return reactiveRedisTemplate.opsForZSet().add(USER_WAIT_QUEUE_KEY.formatted(queue), userId.toString(), unixTimeStamp)
-                .filter(i -> i)
-                .switchIfEmpty(Mono.error(new Exception("already registered user")))
-                .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank(USER_WAIT_QUEUE_KEY.formatted(queue), userId.toString()))
-                .map(i -> i >= 0 ? i + 1 : 0);
-
+                .flatMap(added -> {
+                    if (!added) {
+                        return Mono.error(new ApplicationException(ErrorCode.QUEUE_ALREADY_REGISTERED));
+                    }
+                    return reactiveRedisTemplate.opsForZSet().rank(USER_WAIT_QUEUE_KEY.formatted(queue), userId.toString())
+                            .map(rank -> rank >= 0 ? rank + 1 : 0);
+                });
     }
 
 }
