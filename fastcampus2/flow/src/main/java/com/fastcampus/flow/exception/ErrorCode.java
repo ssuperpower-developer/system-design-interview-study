b22ec6e34dafe03d7135165461fca931f8a043c6
SSUN <rlatjsghks4647@naver.com>
2025-08-11 01:42:39 +0900

    [FIX] 코드 수정

--- fastcampus2/flow/src/main/java/com/fastcampus/flow/exception/ErrorCode.java
+++ fastcampus2/flow/src/main/java/com/fastcampus/flow/exception/ErrorCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum ErrorCode {
-    QUEUE_ALREADY_REGISTERD_USER(HttpStatus.CONFLICT, "UQ-0001", "Already registered queue");
+    QUEUE_ALREADY_REGISTERED_USER(HttpStatus.CONFLICT, "UQ-0001", "Already registered queue");
 
     private final HttpStatus httpStatus;
     private final String code;
--- fastcampus2/flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
+++ fastcampus2/flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
@@ -17,21 +17,17 @@ public class UserQueueService {
 
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
         var unixTimestamp = Instant.now().getEpochSecond();
-
         return reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString(), unixTimestamp)
                 .filter(i -> i)
-                .switchIfEmpty(Mono.error(ErrorCode.QUEUE_ALREADY_REGISTERD_USER.build()))
+                .switchIfEmpty(Mono.error(ErrorCode.QUEUE_ALREADY_REGISTERED_USER.build()))
                 .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString()))
                 .map(i -> i >= 0 ? i + 1 : i);
     }
 
     public Mono<Long> allowUser(final String queue, final Long count) {
         return reactiveRedisTemplate.opsForZSet().popMin(USER_QUEUE_WAIT_KEY.formatted(queue), count)
-                .flatMap(member ->
-                        reactiveRedisTemplate.opsForZSet()
-                                  .add(USER_QUEUE_PROCEED_KEY.formatted(queue), member.getValue(), Instant.now().getEpochSecond()))
+                .flatMap(member -> reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_PROCEED_KEY.formatted(queue), member.getValue(), Instant.now().getEpochSecond()))
                 .count();
-
     }
 
     public Mono<Boolean> isAllowed(final String queue, final Long userId) {
